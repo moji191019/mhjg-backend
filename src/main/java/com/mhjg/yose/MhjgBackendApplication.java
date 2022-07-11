bce83c079d5aac06f838973db03c3a6177119098
bebebio <guslgogo20@gmail.com>
2022-07-11 23:20:43 +0900

    commit

---
+++ src/main/java/com/mhjg/yose/MhjgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mhjg.yose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhjgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhjgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhjg/yose/MhjgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhjg.yose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhjgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
