136c30fcb9b76ec29061df91c74462d902dc1bfc
bebebio <guslgogo20@gmail.com>
2022-07-13 08:07:38 +0900

    mybatis interceptor 추가

--- src/main/java/com/mhjg/yose/db/interceptor/QueryInterceptor.java
+++ src/main/java/com/mhjg/yose/db/interceptor/QueryInterceptor.java
@@ -1,23 +1,33 @@
 package com.mhjg.yose.db.interceptor;
 
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
-@Intercepts(
-		@Signature(
-				args = {}, 
-				method = "", 
-				type = Executor.class)
-		)
+@Intercepts(@Signature(
+	type = Executor.class,
+	method = "query", 
+	args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
+)
 public class QueryInterceptor implements Interceptor {
 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		String statementId = ((MappedStatement) invocation.getArgs()[0]).getId();
+		
+		long start = System.currentTimeMillis();
+		Object resultObj = invocation.proceed();
+		long stop = System.currentTimeMillis() - start;
+		
+		System.out.println("- statement id : " + statementId);
+		System.out.println("- execute time : " + stop);
+		
+		return resultObj;
 	}
 
 }
